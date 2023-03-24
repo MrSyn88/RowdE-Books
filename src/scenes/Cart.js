f1df23b90c138d3ea1ec207128b0f7bb67f8dde4
Colton <coltonmosier44@gmail.com>
2023-03-23 21:39:54 -0500

    Basic cart with navbar refactor

--- src/scenes/Cart.js
+++ src/scenes/Cart.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className='container'>
+      <h1>Cart</h1>
+      <Table striped bordered condensed hover>
+        <thead>
+          <th> # </th>
+          <th> Title </th>
+          <th> Price </th>
+          <th> Quantity </th>
+          <th> Total </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Harry Potter</td>
+            <td>$10.00</td>
+            <td>1</td>
+            <td>$10.00</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Lord of the Rings</td>
+            <td>$10.00</td>
+            <td>1</td>
+            <td>$10.00</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
