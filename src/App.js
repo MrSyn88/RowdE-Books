7759c8b785c3bc941a2699c2b0fb25dcc92fee92
Colton <coltonmosier44@gmail.com>
2023-03-23 22:17:01 -0500

    Auto stash before rebase of "origin/main"

--- src/App.js
+++ src/App.js
@@ -17,44 +17,7 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/Home">RowdE-Books</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Nav.Link as={Link} to="/Login">Login</Nav.Link>
-              
-              <NavDropdown title="Resources" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="https://react-bootstrap.github.io/">React Bootstrap</NavDropdown.Item>
-                <NavDropdown.Item href="https://firebase.google.com/">
-                  Firebase
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="https://github.com/MrSyn88/RowdE-Books">
-                  Our GitHub
-                </NavDropdown.Item>
-              </NavDropdown> 
-              <Nav.Link as={Link} to="/Home" disabled>
-                Books
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <NavBar Link={Link} />
         <Routes>
           <Route exact path="/" element={<Navigate to="/Home" />} />
           <Route path="/Login" element={<Login />} />
