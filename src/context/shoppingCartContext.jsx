9023fa226a1d1eb0d2ae8497739fa8c54538be78
Pete Acevedo <peteacevedo0729@gmail.com>
2023-04-05 16:56:50 -0500

    further attempt at a cart

--- src/context/shoppingCartContext.jsx
+++ src/context/shoppingCartContext.jsx
@@ -11,17 +11,27 @@ export function ShoppingCartProvider({ children }){
     const [cartItems, setCartItems] = useState([])
 
     function getItemQuantity(isbn){
-        return cartItems.find(item => item.isbn === isbn)?.quantity || 0
+        //console.log(cartItems);
+        return cartItems.find(item => item.book.isbn === isbn)?.quantity || 0
     }
 
-    function increaseCartQuantity(isbn){
+    function increaseCartQuantity(isbn, ebook){
+        console.log(cartItems);
+        let book = null;
         setCartItems(currItems => {
-            if(currItems.find(item => item.isbn === isbn) == null) {
-                return [...currItems, {isbn, quantity: 1}]
+            
+            if(currItems.find(item => item.book.isbn === isbn) == null) {
+                
+                
+                //console.log("Fresh Add!");
+                book = ebook;
+                return [...currItems, {book, quantity: 1}]
+                
             }
             else {
                 return currItems.map(item => {
-                    if (item.isbn === isbn) {
+                    if (item.book.isbn === isbn) {
+                        console.log("Found it in the cart!");
                         return {...item, quantity: item.quantity + 1}
                     }
                     else {
@@ -37,12 +47,14 @@ export function ShoppingCartProvider({ children }){
 
     function decreaseCartQuantity(isbn){
         setCartItems(currItems => {
-            if(currItems.find(item => item.isbn === isbn)?.quantity === 1) {
-                return currItems.filter(item => item.isbn !== isbn)
+            if(currItems.find(item => item.book.isbn === isbn)?.quantity === 1) {
+                console.log("removed entirely from cart");
+                return currItems.filter(item => item.book.isbn !== isbn)
             }
             else {
                 return currItems.map(item => {
-                    if (item.isbn === isbn) {
+                    if (item.book.isbn === isbn) {
+                        console.log("Removed from existing cart");
                         return {...item, quantity: item.quantity - 1}
                     }
                     else {
@@ -55,7 +67,8 @@ export function ShoppingCartProvider({ children }){
 
     function removeFromCart(isbn){
         setCartItems(currItems => {
-            return currItems.filter(item => item.isbn !== isbn)
+            console.log("Removed through red button!");
+            return currItems.filter(item => item.book.isbn !== isbn)
         })
     }
 
