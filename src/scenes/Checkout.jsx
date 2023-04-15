e2da3f132a092bc461c8d190334e495057eef40d
Colton Mosier <coltonmosier44@gmail.com>
2023-04-15 12:34:54 -0500

    feat: remove from cart/checkout, adjust size/style

--- src/scenes/Checkout.jsx
+++ src/scenes/Checkout.jsx
@@ -1,71 +1,65 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
-import Carousel from 'react-bootstrap/Carousel';
-import Popover from 'react-bootstrap/Popover';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Image from 'react-bootstrap/Image'
-import { db } from "../firebase";
-import book1 from '../images/bookPhoto-1.jpg'
-import book2 from '../images/bookPhoto-2.jpg'
-import book3 from '../images/bookPhoto-3.jpg'
-import book4 from '../images/books4.jpg'
-import firstSlide from '../images/Carousel1.jpg'
-import secondSlide from '../images/carousel2.jpg'
-import thirdSlide from '../images/carousel3.jpg'
-import utsa from '../images/utsa-logo.png'
-import { collection, getDocs } from 'firebase/firestore';
 import { useShoppingCart } from '../context/shoppingCartContext';
-import { Stack } from "react-bootstrap";
 import { CheckoutItem } from '../component/CheckoutItem';
 
 
 const Checkout = () => {
 
-const {cartItems} = useShoppingCart();
+    const { cartItems } = useShoppingCart();
 
-cartItems.map(({book,quantity}) => (
-    console.log(book, quantity)
-));
-const cartSubtotal = cartItems.reduce(
-    (acc, item) => acc + item.book.price * item.quantity,
-    0
-  );
+    cartItems.map(({ book, quantity }) => (
+        console.log(book, quantity)
+    ));
+    const cartSubtotal = cartItems.reduce(
+        (acc, item) => acc + item.book.price * item.quantity,
+        0
+    );
 
-  const tax = cartSubtotal * 0.0825;
-  const total = cartSubtotal + tax;
+    const tax = cartSubtotal * 0.0825;
+    const total = cartSubtotal + tax;
 
-return (
-    <Container>
+    return (
+        <Container>
+            {cartItems.length === 0 ? (
+                <>
+                    <h1 style={{ color: "#FFFFFF" }}>Your cart is empty!</h1>
+                    <hr style={{ color: 'white' }} />
+                    <Button variant="primary" href="/Books">Books</Button>
+                </>
+            ) : (
+                <div>
+                    <h1 style={{ color: "#FFFFFF" }}>Finalize Your Purchase Now!</h1>
+                    <hr style={{ color: 'white' }} />
 
-        <h1 style={{color:"#FFFFFF"}}>Finalize Your Purchase Now!</h1>
-    
-        <Row>
-            
-            {cartItems.map(({ book, quantity}) => (
-                //I want to calculate the running subtotal for each book and then add them all up to get the total
-                <CheckoutItem key={book.isbn} isbn={book.isbn} quantity={quantity} /> // working cart screen
-            ))}
-            
-    </Row>
+                    <Row>
 
-            <h3 style={{color:"#FFFFFF"}}>Subtotal: ${cartSubtotal.toFixed(2)}</h3>
-            <h3 style={{color:"#FFFFFF"}}>Tax: ${tax.toFixed(2)}</h3>
-            <h3 style={{color:"#FFFFFF"}}>Total: ${total.toFixed(2)}</h3>
-            
-            <Button
-                        className="ms-3 mt-1"
-                        style={{}}
-                        variant="primary"
-                    >
-                        Input Credit Card Info
-                    </Button>    
-    </Container>
-);
+                        {cartItems.map(({ book, quantity }) => (
+                            //I want to calculate the running subtotal for each book and then add them all up to get the total
+                            <CheckoutItem key={book.isbn} isbn={book.isbn} quantity={quantity} /> // working cart screen
+                        ))}
+
+                    </Row>
+                <hr style={{ color: 'white' }} />
+                    <div className='mt-3'>
+                        <h3 style={{ color: "#FFFFFF" }}>Subtotal: ${cartSubtotal.toFixed(2)}</h3>
+                        <h3 style={{ color: "#FFFFFF" }}>Tax: ${tax.toFixed(2)}</h3>
+                        <h3 style={{ color: "#FFFFFF" }}>Total: ${total.toFixed(2)}</h3>
+
+                        <Button
+                            className="ms-3 mt-1"
+                            style={{}}
+                            variant="primary"
+                        >
+                            Input Credit Card Info
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </Container>
+    );
 
 
 }
