63c7f667ae9a4f7666336319b1dd9e9845f314bd
Croaxis <112976203+Croaxis@users.noreply.github.com>
2023-04-19 14:21:37 -0500

    Added main changes for NavBar.jsx manually

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -4,18 +4,35 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { logout, signInWithGoogle } from '../firebase';
+import { isAdmin, logout, signInWithGoogle } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom'
+import { useShoppingCart } from '../context/shoppingCartContext';
+import logo from '../images/rowde-books-low-resolution-logo-white-on-transparent-background.png'
 
 
 export default function NavBar({ Link }) {
   const [user] = useAuthState(auth);
+  const { openCart, cartQuantity } = useShoppingCart();
+  const [admin, setAdmin] = useState(false);
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState("");
 
+  const sendEmHome = () => {
+    return <Navigate to="/Home" />
+  }
+
+  // Check if user is admin
+  if (user) {
+    isAdmin(user.uid).then((value) => {
+      if (value === true) {
+        setAdmin(true);
+      }
+    })
+  }
 
   const handleSearch = () => {
     window.localStorage.setItem('searchbarSubmittedText', JSON.stringify(searchValue));
@@ -24,13 +41,27 @@ export default function NavBar({ Link }) {
 
   return (<Navbar bg="dark" expand="lg" variant='dark' sticky="top">
     <Container fluid>
-      <Navbar.Brand as={Link} to="/Home">RowdE-Books</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/Home">
+        <img
+          src={logo}
+          width="234.375"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
         <Nav className="me-auto my-2 my-lg-0" style={{
           maxHeight: '100px'
         }} navbarScroll>
-
+          <Nav.Link as={Link} to="/Books" >
+            Books
+          </Nav.Link>
+          {/*<Nav.Link as={Link} to="/Cart">Cart</Nav.Link>*/}
+          {user && admin === true ?
+            <Nav.Link as={Link} to="/Admin">Admin</Nav.Link>
+            : null}
           <NavDropdown title="Resources" id="navbarScrollingDropdown">
             <NavDropdown.Item href="https://react.dev" target="_blank">React</NavDropdown.Item>
             <NavDropdown.Item href="https://react-bootstrap.github.io/" target="_blank">React Bootstrap</NavDropdown.Item>
@@ -42,11 +73,13 @@ export default function NavBar({ Link }) {
               Our GitHub
             </NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link as={Link} to="/Books" >Books</Nav.Link>
-          <Nav.Link as={Link} to="/Cart">Cart</Nav.Link>
         </Nav>
+        <Button style={{ width: "4rem", height: "3rem", position: "relative" }} variant="outline-primary" className='' onClick={openCart}>
+          Cart
+          <div style={{ width: "1.5rem", color: "white", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)" }} className='rounded-circle bg-danger d-flex justify-content-center align-items-center'>{cartQuantity}</div>
+        </Button>
         <Form className="d-flex" onSubmit={handleSearch} >
-          <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" onChange={(event => setSearchValue(event.target.value))}/>
+          <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" onChange={(event => setSearchValue(event.target.value))} />
           <Button variant="outline-success" onClick={handleSearch} onChange={(event => setSearchValue(event.target.value))}>Search</Button>
         </Form>
         <Form className="d-flex">
@@ -59,12 +92,17 @@ export default function NavBar({ Link }) {
             </Button>
           ) : (
             <>
-              <Button
-                variant="outline-primary"
-                onClick={logout}
-              >
-                Log out
-              </Button>
+              <Link to="/Home">
+                <Button
+                  variant="outline-danger"
+                  onClick={() => {
+                    sendEmHome()
+                    logout()
+                  }}
+                >
+                  Log out
+                </Button>
+              </Link>
             </>
           )}
         </Form>
